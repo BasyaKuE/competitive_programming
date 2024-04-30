bac309df66953fb8baa2ab85cccabb38b52534f3
添田英紀 <soeta-eiki@ed.tmu.ac.jp>
2024-05-01 04:28:36 +0900

    精進

--- atcoder/abc/abc349/c/main.py
+++ atcoder/abc/abc349/c/main.py
@@ -2,3 +2,20 @@ import bisect
 import itertools
 import sys
 from typing import List
+
+S = input()
+T = input()
+
+chk = ""
+chk_i = 0
+for i in range(len(S)):
+    if S[i].upper() == T[chk_i]:
+        chk += S[i]
+        chk_i += 1
+    if chk_i >= 3:
+        break
+# print(chk)
+if chk.upper() == T or chk[0:2].upper()+"X" == T:
+    print("Yes")
+else:
+    print("No")
--- atcoder/abc/abc351/d/main.py
+++ atcoder/abc/abc351/d/main.py
@@ -1,6 +1,23 @@
 H, W = map(int, input().split())
 grid = [input() for _ in range(H)]
 
+state = [[1 for _ in range(W)] for _ in range(H)]
+# 0磁石, 1ok, 2隣接
+directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+for i in range(H):
+    for j in range(W):
+        if grid[i][j] == "#":
+            state[i][j] = 0
+        else:
+            for dx, dy in directions:
+                nx, ny = i + dx, j + dy
+                if 0 <= nx < H and 0 <= ny < W and grid[nx][ny] == '#':
+                    grid[nx][ny] = 2
+
+for s in state:
+    print(s)
+
 directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 def dfs_stack(x, y, visited):
